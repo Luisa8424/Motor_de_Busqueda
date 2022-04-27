d0246c0df421f5dfa543481a30673f00f1fbd88a
dianamromeroc <101436143+dianamromeroc@users.noreply.github.com>
2022-04-27 00:39:03 -0500

    clase usuario final

---
+++ clase usuario.py
@@ -0,0 +1,10 @@
+class Usuario:
+    str Id
+    str Nombre
+    str TipoContratacion
+    str HorasDedicacion
+    str Cargo
+        
+    
+    def __init__(self):
+        self.Departamento = Departamento()
