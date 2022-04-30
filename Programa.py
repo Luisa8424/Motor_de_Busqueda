b8420cf6960e07628ba7fa9fcc12b2b4a3e9a52a
Luisa8424 <101436183+Luisa8424@users.noreply.github.com>
2022-04-30 16:41:32 -0500

    Clase Programa

--- Programa.py
+++ Programa.py
@@ -2,10 +2,11 @@ class Programa :
     str IdPrograma
     str NomPrograma
     Certifacion:Certifacion ()
-    Asignatura:Asignatura ()
-def _int_(self, Facultad, Asignatura, Grupo):
+    
+def _int_(self, Facultad, Asignatura, Grupo, Estudiante):
     self.Facultad=Facultad ()
     self.Asignatura=Asignatura ()
     self.Grupo=Grupo
+    self.Estudiante=Estudiante
 
     
