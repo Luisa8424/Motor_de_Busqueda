9a1503d9bb77ddf0fa6ceb552e9fe7423b3be529
dianamromeroc <101436143+dianamromeroc@users.noreply.github.com>
2022-05-03 01:42:48 -0500

    Actualización

--- Clase ExperienciaInvestigacion.py
+++ Clase ExperienciaInvestigacion.py
@@ -12,24 +12,24 @@ def ActualizarExperienciaInvestigacion(self, cantidaPublicaciones: int, tipoPubl
          """Mostrar """
          return cantidaPublicaciones: int, tipoPublicion: str
      
-   f = open("usuarios.txt", "w")
-   f.write(anosInvestigacion:int, cantidadPublicaciones: int, tipoPublicacion: str, docente: Docente)
-   87
-   f.close()
-   f=open("usuarios.txt", "a")
-   f.write("agregar anosInvestigacion, cantidadPublicaciones, tipoPublicaion, docente")
-   52
-   f.close()
-   f = open('usuarios.txt', 'r')
-   lines = f.readlines()
+f = open("usuarios.txt", "w")
+f.write(anosInvestigacion:int, cantidadPublicaciones: int, tipoPublicacion: str, docente: Docente)
+87
+f.close()
+f=open("usuarios.txt", "a")
+f.write("agregar anosInvestigacion, cantidadPublicaciones, tipoPublicaion, docente")
+52
+f.close()
+f = open('usuarios.txt', 'r')
+lines = f.readlines()
    
-   for i in range(len(lines)):
+for i in range(len(lines)):
        lines[i] = lines[i].replace('\n', '')
        lista_anosPublicacion_cantidadPublicaiones_tipoPublicacion_docente = []
-   for i in lines:
+for i in lines:
        lista_anosPublicacion_cantidadPublicaiones_tipoPublicacion_docente.append(i.split(" "))
        publicaciones = []
-   for i in lista_nombreGrupo_fechaGrupoinvestigacion_docente:
+for i in anosPublicacion_cantidadPublicaiones_tipoPublicacion_docente:
        publicaciones.append(i[0])
        publicaciones.sort()
        print(publicaciones)
