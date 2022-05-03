1ee3dad4502bec5df91a29cd9eaae4b45102e83f
Luisa8424 <101436183+Luisa8424@users.noreply.github.com>
2022-05-02 23:52:31 -0500

    Clase Facultad

--- Experiencia Docente.py
+++ Experiencia Docente.py
@@ -8,3 +8,10 @@ class ExperienciaDocente:
     def ExperienciaDocente(self):
         print("Actualizar Docente Tipo Experiencia.")
 ExperienciaDocente.ExperienciaDocente()
+
+Archivo_Docente=open("Texto_Docente.txt", "w")
+Archivo_Docente.write("Motor de Búsqueda")
+Archivo_Docente.write("Construimos este archivo\n")
+Archivo_Docente.write("Para que nos arroje datos\n")
+Archivo_Docente.write("Sobre los docentes de la Universidad y validar si están certificados\n")
+Archivo_Docente.close()
--- Facultad.py
+++ Facultad.py
@@ -8,3 +8,10 @@ class Facultdad:
     def BuscarFacultad(self):
         print("Definir Facultad.")
 Facultdad.BuscarFacultad()
+
+Archivo_Facultad=open("Texto_Facultad.txt", "w")
+Archivo_Facultad.write("Motor de Búsqueda")
+Archivo_Facultad.write("Construimos este archivo\n")
+Archivo_Facultad.write("Para que nos arroje datos\n")
+Archivo_Facultad.write("Sobre las facultades de la Universidad\n")
+Archivo_Facultad.close()
