69faa517d736788ec3ef7e42cbd8e101fb118c7a
dianamromeroc <101436143+dianamromeroc@users.noreply.github.com>
2022-05-02 23:58:58 -0500

    actualización

--- Clase Usuario2.py
+++ Clase Usuario2.py
@@ -1,3 +1,6 @@
+from inspect import _void
+
+
 class Usuario:
     Id:str
     Nombre:str
@@ -8,3 +11,38 @@ class Usuario:
     
     def __init__(self,Departamento):
         self.Departamento = Departamento()
+        
+           
+    def ActualizarUsuario(self,id:str, nombre: str, tipoContratacion: str, maximoNivelformacion:str, cargo: str ):
+...         """Mostrar """
+...         return id:str, nombre: str, tipoContratacion: str, maximoNivelformacion:str, cargo: str
+
+    def CrearUsuario(self,void):
+...         """Mostrar """
+...         return _void
+       f = open("usuarios.txt", "w")
+       f.write(id:str, nombre: str, tipoContratacion: str, maximoNivelformacion:str, cargo: str)
+       80
+       f.close()
+       f=open("usuarios.txt", "a")
+       f.write("agregar usuario")
+       15
+       f.close()
+       f = open('usuarios.txt', 'r')
+       
+       lines = f.readlines()
+       
+       
+       for i in range(len(lines)):
+           lines[i] = lines[i].replace('\n', '')
+           
+           lista_id_nombre_tipoContratacion_maximoNivelFormacion_cargo = []
+           for i in lines:
+               lista_id_nombre_tipoContratacion_maximoNivelFormacion_cargo.append(i.split(" "))
+               nombres = []
+               for i in lista_id_nombre_tipoContratacion_maximoNivelFormacion_cargo:
+                   nombres.append(i[0])
+                   nombres.sort()
+                   print(nombres)
+                   f.close()
+       
