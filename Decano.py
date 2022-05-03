d3513df972b7a08242251c8b6c59ce2ef145ed89
Luisa8424 <101436183+Luisa8424@users.noreply.github.com>
2022-05-02 22:53:03 -0500

    Clase Decano

--- Decano.py
+++ Decano.py
@@ -2,3 +2,7 @@ class Decano (Usuario):
     Funciones:str
     def _int_(self, Facultad):
         self.Facultad=Facultad
+    
+    def BuscarDecano(self):
+        print("Modificar Decano.")
+Decano.BuscarDecano()
