7246a420202b35dd7e2878ca49e2f0a69d27b694
dianamromeroc <101436143+dianamromeroc@users.noreply.github.com>
2022-05-03 01:39:08 -0500

    Actualización

--- Clase Formacion.py
+++ Clase Formacion.py
@@ -8,35 +8,35 @@ class Formacion:
     
         
     
-    def __init__(self, Docente):
+def __init__(self, Docente):
         self.Docente = Docente()
         
-    def BuscarFormacion(self,codFormacion:str, tipoFormacion: str, nombreFormacion: str ):
+def BuscarFormacion(self,codFormacion:str, tipoFormacion: str, nombreFormacion: str ):
          """Mostrar """
-       return codFormacion:str, tipoFormacion: str, nombreFormacion: str
+         return codFormacion:str, tipoFormacion: str, nombreFormacion: str
    
-    def DefinirFormacion(self):
+def DefinirFormacion(self):
          """Mostrar """
-       return DefinirFormacion
+         return DefinirFormacion
      
-   f = open("usuarios.txt", "w")
-   f.write(codFormacion:str, tipoFormacion: str, nombreFormacion: str)
-   56
-   f.close()
-   f=open("usuarios.txt", "a")
-   f.write("agregar codFormacion, tipoFormacion, nombreFormacion")
-   52
-   f.close()
-   f = open('usuarios.txt', 'r')
-   lines = f.readlines()
+f = open("usuarios.txt", "w")
+f.write(codFormacion:str, tipoFormacion: str, nombreFormacion: str)
+56
+f.close()
+f=open("usuarios.txt", "a")
+f.write("agregar codFormacion, tipoFormacion, nombreFormacion")
+52
+f.close()
+f = open('usuarios.txt', 'r')
+lines = f.readlines()
    
-   for i in range(len(lines)):
+for i in range(len(lines)):
        lines[i] = lines[i].replace('\n', '')
        lista_codFormacion_tipoFormacion_nombreFormacion = []
-   for i in lines:
+for i in lines:
        lista_codFormacion_tipoFormacion_nombreFormacion.append(i.split(" "))
        formacion = []
-   for i in lista_nombreGrupo_fechaGrupoinvestigacion_docente:
+for i in lista_codFormacion_tipoFormacion_nombreFormacion:
        formacion.append(i[0])
        formacion.sort()
        print(formacion)
