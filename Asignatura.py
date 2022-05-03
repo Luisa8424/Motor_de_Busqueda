82f05f48de82e2039e453657c6075b5caa3e9280
Luisa8424 <101436183+Luisa8424@users.noreply.github.com>
2022-05-02 22:57:52 -0500

    Clase Asignatura

--- Asignatura.py
+++ Asignatura.py
@@ -7,4 +7,11 @@ class Asignatura:
     def _int_(self, Estudiante, Programa,):
         self.Estudiante=Estudiante()
         self.Programa=Programa()
-    
+
+        def BuscarAsignatura(self):
+            def CrearAsignatura(self):
+                def ModificarAsignatura(self):
+                    print("Definir Asignatura.")
+Asignatura.BuscarAsignatura()
+Asignatura.CrearAsignatura()
+Asignatura.ModificarAsignatura()
