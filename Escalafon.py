73f1b998f397f40ba65ed1fe55c7c2385b04ed5f
sergi0623 <sducuaral94911@umanizales.edu.co>
2022-05-03 17:36:00 -0500

    corrijo clase

--- Escalafon.py
+++ Escalafon.py
@@ -13,3 +13,11 @@ class Escalafon:
 
 Escalafon.BuscarEscalafon()
 Escalafon.DefinirEscalafon()
+
+Archivo_3=open("Texto Escalafon", "w")
+Archivo_3.write("MOTOR DE BUSQUEDA")
+Archivo_3.write("Por medio de este texto \n")
+Archivo_3.write("almacenaremos todos los datos \n")
+Archivo_3.write("que sean consultados para \n")
+Archivo_3.write("la clase escalafon.")
+Archivo_3.close()
