4f020a056aeed190d9e61742dd44efa24b76f564
dianamromeroc <101436143+dianamromeroc@users.noreply.github.com>
2022-05-03 01:18:29 -0500

    Actualización

--- Clase Usuario2.py
+++ Clase Usuario2.py
@@ -9,40 +9,37 @@ class Usuario:
     HorasDedicacion:str
     Cargo:str
     
-    def __init__(self,Departamento):
+def __init__(self,Departamento):
         self.Departamento = Departamento()
         
            
-    def ActualizarUsuario(self,id:str, nombre: str, tipoContratacion: str, maximoNivelformacion:str, cargo: str ):
-...         """Mostrar """
-...         return id:str, nombre: str, tipoContratacion: str, maximoNivelformacion:str, cargo: str
+def ActualizarUsuario(self,id:str, nombre: str, tipoContratacion: str, maximoNivelformacion:str, cargo: str ):
+       """Mostrar """
+       return id:str, nombre: str, tipoContratacion: str, maximoNivelformacion:str, cargo: str
 
-    def CrearUsuario(self,void):
-...         """Mostrar """
-...         return _void
-       f = open("usuarios.txt", "w")
-       f.write(id:str, nombre: str, tipoContratacion: str, maximoNivelformacion:str, cargo: str)
-       80
-       f.close()
-       f=open("usuarios.txt", "a")
-       f.write("agregar usuario")
-       15
-       f.close()
-       f = open('usuarios.txt', 'r')
+def CrearUsuario(self,void):
+        """Mostrar """
+        return _void
+    f = open("usuarios.txt", "w")
+    f.write(id:str, nombre: str, tipoContratacion: str, maximoNivelformacion:str, cargo: str)
+    80
+    f.close()
+    f=open("usuarios.txt", "a")
+    f.write("agregar usuario")
+    15
+    f.close()
+    f = open('usuarios.txt', 'r')
        
-       lines = f.readlines()
-       
-       
-       for i in range(len(lines)):
-           lines[i] = lines[i].replace('\n', '')
-           
-           lista_id_nombre_tipoContratacion_maximoNivelFormacion_cargo = []
-           for i in lines:
-               lista_id_nombre_tipoContratacion_maximoNivelFormacion_cargo.append(i.split(" "))
-               nombres = []
-               for i in lista_id_nombre_tipoContratacion_maximoNivelFormacion_cargo:
-                   nombres.append(i[0])
-                   nombres.sort()
-                   print(nombres)
-                   f.close()
-       
+    lines = f.readlines()
+    
+    for i in range(len(lines)):
+        lines[i] = lines[i].replace('\n', '')
+        lista_id_nombre_tipoContratacion_maximoNivelFormacion_cargo = []
+    for i in lines:
+        lista_id_nombre_tipoContratacion_maximoNivelFormacion_cargo.append(i.split(" "))
+        nombresusuarios = []
+    for i in lista_id_nombre_tipoContratacion_maximoNivelFormacion_cargo:
+        nombresusuarios.append(i[0])
+        nombresusuarios.sort()
+        print(nombresusuarios)
+        f.close()     
